8587202fee6cb6a3c7964955b5b3dbfa11ae22b2
sandeshsitaula <navaltajohan@gmail.com>
2024-02-14 20:09:38 +0545

    formatting done

--- frontend/src/components/CutVideoModal.jsx
+++ frontend/src/components/CutVideoModal.jsx
@@ -1,87 +1,95 @@
-  import {Modal,FormControl,Form,Col,Button} from 'react-bootstrap'
-import {useState} from 'react'
-import axiosInstance from '../axiosInstance'
- export const CutVideoModal=(props)=>{
-    const [loading, setLoading] = useState(false);
+import { Modal, FormControl, Form, Col, Button } from "react-bootstrap";
+import { useState } from "react";
+import axiosInstance from "../axiosInstance";
+export const CutVideoModal = (props) => {
+  const [loading, setLoading] = useState(false);
 
-    function handleChange(e){
-    props.setSubtitleToCut(e.target.value)
-    console.log(e.target.value)
-    }
+  function handleChange(e) {
+    props.setSubtitleToCut(e.target.value);
+    console.log(e.target.value);
+  }
 
-    async function handleSubmit() {
+  async function handleSubmit() {
     if (loading) {
       return;
     }
-    if (props.subtitleToCut.length<=0){
-        alert("enter some subtitle to cut")
-        return
+    if (props.subtitleToCut.length <= 0) {
+      alert("enter some subtitle to cut");
+      return;
     }
     setLoading(true);
     try {
-     const postData={
-         'subtitleToCut':props.subtitleToCut,
-        'videoName':props.videoName,
-
-      }
-      const response =await axiosInstance.post('cutvideo/',postData)
-      console.log(response.data)
+      const postData = {
+        subtitleToCut: props.subtitleToCut,
+        videoName: props.videoName,
+      };
+      const response = await axiosInstance.post("cutvideo/", postData);
+      console.log(response.data);
       alert(response.data.message);
-      props.setOriginalVideoName(response.data.original_video)
-      props.setCutVideoName(response.data.cut_video)
+      props.setOriginalVideoName(response.data.original_video);
+      props.setCutVideoName(response.data.cut_video);
       setLoading(false);
-
     } catch (error) {
-      console.log(error)
-      if (error.response){
-        alert(error.response.data.message)
-      }else{
-        alert(error)
+      console.log(error);
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert(error);
       }
       setLoading(false);
     }
   }
-      const handleClose = () => { props.setModal(false) };
-      return(
-          <>
-          <Modal show={true} size="xl" onHide={handleClose}  centered
-backdrop="static"
-keyboard={false}>
-        <Modal.Header closeButton>
-        </Modal.Header>
-        <Modal.Body style={{display:'flex',justifyContent:'center', padding:
-"0px" }}>
-         <Col sm={9} md={5}>
-              <div style={{ backgroundColor: "white" }}>
-                <h5 style={{ textAlign: "center", paddingBottom: "20px" }}>
-                  Cut Video
-                </h5>
+  const handleClose = () => {
+    props.setModal(false);
+  };
+  return (
+    <>
+      <Modal
+        show={true}
+        size="xl"
+        onHide={handleClose}
+        centered
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body
+          style={{ display: "flex", justifyContent: "center", padding: "0px" }}
+        >
+          <Col sm={9} md={5}>
+            <div style={{ backgroundColor: "white" }}>
+              <h5 style={{ textAlign: "center", paddingBottom: "20px" }}>
+                Cut Video
+              </h5>
 
-                  <div style={{textAlign:'center',marginBottom:'4rem'}}>
-                  <Form.Control
+              <div style={{ textAlign: "center", marginBottom: "4rem" }}>
+                <Form.Control
                   placeholder="Paste the part of generated subtitle"
                   as="textarea"
-                  onChange={(e)=>{handleChange(e)}}
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
                   value={props.subtitleToCut}
-                  rows={5} />
+                  rows={5}
+                />
 
-                      <Button
-                        disabled={loading}
-                        style={{ width: "75%",marginButton:'4rem'
-,marginTop:'2rem'}}
-                        variant="outline-success"
-                        onClick={handleSubmit}
-
-                      >
-                        {loading ? "Loading" : "Cut Video"}
-                      </Button>
-                      </div>
-        </div>
-        </Col>
+                <Button
+                  disabled={loading}
+                  style={{
+                    width: "75%",
+                    marginButton: "4rem",
+                    marginTop: "2rem",
+                  }}
+                  variant="outline-success"
+                  onClick={handleSubmit}
+                >
+                  {loading ? "Loading" : "Cut Video"}
+                </Button>
+              </div>
+            </div>
+          </Col>
         </Modal.Body>
-        </Modal>
-        </>
-    )
-}
-
-
+      </Modal>
+    </>
+  );
+};
--- frontend/src/components/UploadForm.jsx
+++ frontend/src/components/UploadForm.jsx
@@ -1,32 +1,33 @@
 // UploadForm.js
-import  { useState ,useEffect} from 'react';
-import {Button} from 'react-bootstrap'
-import axiosInstance from '../axiosInstance'
-import {CutVideoModal} from './CutVideoModal'
+import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import axiosInstance from "../axiosInstance";
+import { CutVideoModal } from "./CutVideoModal";
 
 const CHUNK_SIZE = 10 * 1024 * 1024; // 10MB chunk size
 
 const UploadForm = () => {
-
   const [file, setFile] = useState(null);
   const [uploading, setUploading] = useState(false);
-  const [videoName,setVideoName]=useState('')
-  const [subtitleData,setSubtitleData]=useState('')
-  const [showModal,setShowModal]=useState(false)
-  const [subtitleToCut,setSubtitleToCut]=useState('')
-  const [originalVideoName,setOrignalVideoName]=useState('')
-  const [cutVideoName,setCutVideoName]=useState('')
-  const [loading,setLoading]=useState(false)
+  const [videoName, setVideoName] = useState("");
+  const [subtitleData, setSubtitleData] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [subtitleToCut, setSubtitleToCut] = useState("");
+  const [originalVideoName, setOrignalVideoName] = useState("");
+  const [cutVideoName, setCutVideoName] = useState("");
+  const [loading, setLoading] = useState(false);
 
-
-  useEffect(()=>{
-    console.log(originalVideoName,cutVideoName)
-  })
+  useEffect(() => {
+    console.log(originalVideoName, cutVideoName);
+  });
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
-    var tempVideoName=event.target.files[0].name.split('.').slice(0,-1).join(".")
-    console.log(tempVideoName)
-    setVideoName(tempVideoName)
+    var tempVideoName = event.target.files[0].name
+      .split(".")
+      .slice(0, -1)
+      .join(".");
+    console.log(tempVideoName);
+    setVideoName(tempVideoName);
   };
 
   const handleUpload = async () => {
@@ -37,107 +38,127 @@ const UploadForm = () => {
       var start = 0;
       var end = Math.min(CHUNK_SIZE, file.size);
       let chunkNumber = 0;
-      let response
+      let response;
       while (start < file.size) {
         const formData = new FormData();
-        formData.append('video', file.slice(start, end));
-        formData.append('videoName', videoName);
+        formData.append("video", file.slice(start, end));
+        formData.append("videoName", videoName);
 
         // Add metadata to identify the chunk
-        formData.append('chunkNumber', chunkNumber);
-        formData.append('totalChunks', Math.ceil(file.size / CHUNK_SIZE));
+        formData.append("chunkNumber", chunkNumber);
+        formData.append("totalChunks", Math.ceil(file.size / CHUNK_SIZE));
 
-         response=await axiosInstance.post('videoupload/',formData ) ;
+        response = await axiosInstance.post("videoupload/", formData);
 
         chunkNumber++;
         start = end;
         end = Math.min(start + CHUNK_SIZE, file.size);
       }
 
-//       console.log(response.data)
-      setSubtitleData(response.data.data)
-      console.log('Upload complete');
+      //       console.log(response.data)
+      setSubtitleData(response.data.data);
+      console.log("Upload complete");
     } catch (error) {
-      alert(error.error)
-      console.error('Error uploading chunk:', error);
+      alert(error.error);
+      console.error("Error uploading chunk:", error);
     } finally {
       setUploading(false);
     }
   };
 
-   const handleFileDownload=async(target)=>{
-     console.log('hello')
-    if (loading){
-      alert("In progress.Wait a while")
-      return
+  const handleFileDownload = async (target) => {
+    console.log("hello");
+    if (loading) {
+      alert("In progress.Wait a while");
+      return;
     }
-    setLoading(true)
-    try{
-        const downloadUrl = `http://meet.fractalnetworks.co:8000/api/filedownload/${target}`;
+    setLoading(true);
+    try {
+      const downloadUrl = `http://meet.fractalnetworks.co:8000/api/filedownload/${target}`;
 
-    // Create a dynamic <a> tag
-    const link = document.createElement('a');
-    link.href = downloadUrl;
-    link.setAttribute('download', ''); // Set the download attribute to trigger download
-    document.body.appendChild(link);
+      // Create a dynamic <a> tag
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.setAttribute("download", ""); // Set the download attribute to trigger download
+      document.body.appendChild(link);
 
-    // Initiate the download
-    link.click();
-
+      // Initiate the download
+      link.click();
 
       setLoading(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       setLoading(false);
-      if (error.response){
-        alert(error.response.data.message)
-      }else{
-        alert(error)
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert(error);
       }
-  }
-  }
-
+    }
+  };
 
   return (
     <>
-    {showModal && <CutVideoModal videoName={videoName}
-subtitleToCut={subtitleToCut} setSubtitleToCut={setSubtitleToCut}
-setModal={setShowModal} setOriginalVideoName={setOrignalVideoName}
-setCutVideoName={setCutVideoName} />}
+      {showModal && (
+        <CutVideoModal
+          videoName={videoName}
+          subtitleToCut={subtitleToCut}
+          setSubtitleToCut={setSubtitleToCut}
+          setModal={setShowModal}
+          setOriginalVideoName={setOrignalVideoName}
+          setCutVideoName={setCutVideoName}
+        />
+      )}
 
-    <div style={{backgroundColor:'#242424'}}>
-    <div style={{color:'white'}}
-className="fileUpload">
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload} disabled={uploading}>
-        {uploading ? 'Uploading...' : 'Submit'}
-      </button>
-    </div>
-    {subtitleData &&<div style={{marginTop:'5rem',color:'white'}}
-className="subtitleText">
+      <div style={{ backgroundColor: "#242424" }}>
+        <div style={{ color: "white" }} className="fileUpload">
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={handleUpload} disabled={uploading}>
+            {uploading ? "Uploading..." : "Submit"}
+          </button>
+        </div>
+        {subtitleData && (
+          <div
+            style={{ marginTop: "5rem", color: "white" }}
+            className="subtitleText"
+          >
+            <div style={{ textAlign: "center" }}>
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Cut Video
+              </Button>
+              {originalVideoName.length > 0 && cutVideoName.length > 0 && (
+                <div
+                  style={{
+                    display: "flex",
+                    marginTop: "2rem",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Button
+                    variant="primary"
+                    onClick={() => handleFileDownload(originalVideoName)}
+                    disabled={loading}
+                  >
+                    Download Original Video
+                  </Button>
+                  <Button
+                    style={{ marginLeft: "2rem" }}
+                    variant="primary"
+                    onClick={() => handleFileDownload(cutVideoName)}
+                    disabled={loading}
+                  >
+                    Download Cut Video
+                  </Button>
+                </div>
+              )}
+            </div>
 
-    <div style={{textAlign:'center'}}>
-      <Button variant="primary" onClick={()=>setShowModal(true)}>Cut
-Video</Button>
-{originalVideoName.length>0 && cutVideoName.length>0 &&  <div
-style={{display:'flex',marginTop:'2rem',justifyContent:'center'}}>
+            <h4>Generated Subtitles</h4>
 
-   <Button  variant="primary" onClick={()=>handleFileDownload(originalVideoName)} disabled={loading}>Download Original
-Video</Button>
-   <Button  variant="primary" onClick={()=>handleFileDownload(cutVideoName)} disabled={loading}>Download Cut
-Video</Button>
-    </div>
-}
-    </div>
-
-    <h4>Generated Subtitles</h4>
-
-    <p style={{marginTop:'2rem'}}>
-    {subtitleData}
-    </p>
-
-    </div>}
-    </div>
+            <p style={{ marginTop: "2rem" }}>{subtitleData}</p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
