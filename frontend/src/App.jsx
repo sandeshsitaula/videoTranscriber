13672e4eb52a623e47a2b98ce641d11a9c3d8c95
sandeshsitaula <navaltajohan@gmail.com>
2024-02-22 20:48:40 +0545

    init for captureevent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { OriginalVideoList } from "./pages/OriginalVideoList";
+import {CaptureEvent} form './pages/CaptureEvent'
 import { CutVideoList } from "./pages/CutVideoList";
 import Video from "./components/videoPlayer/Video";
 import UploadForm from "./components/cutVideo/UploadForm";
@@ -22,6 +22,7 @@ function App() {
               element={<UploadForm handleFileUpload={handleFileUpload} />}
             />
             <Route path="/cutvideolist/:video_id" element={<CutVideoList />} />
+            <Route path="/captureevent" element={<CaptureEvent handleFileUpload={handleFileUpload} />}>
           </Routes>
         </Router>
       }
