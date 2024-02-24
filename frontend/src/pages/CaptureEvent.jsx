b594b7bdafe63e6815e77818861e07f7fbfd13f6
sandeshsitaula <navaltajohan@gmail.com>
2024-02-24 07:29:58 +0545

    added conditional code for checking whether back camera exists or not

--- frontend/src/pages/CaptureEvent.jsx
+++ frontend/src/pages/CaptureEvent.jsx
@@ -5,25 +5,58 @@ export function CaptureEvent(props){
   const [uploading,setUploading]=useState(false)
   const [mediaRecorder, setMediaRecorder] = useState(null);
   const [recordedChunks, setRecordedChunks] = useState([]);
+  const [facingMode,setFacingMode]=useState('user')
+  const [isIOS, setIsIOS] = useState(false);
+  const [backCameraExists,setBackCameraExists]=useState(true)
   const videoRef = useRef(null);
+  useEffect(() => {
+
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    setIsIOS(userAgent.includes('iphone') || userAgent.includes('ipad'));
+    async function cameraChecker(){
+    // Enumerate devices to find the back camera
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const backCamera = devices.find(device => device.kind === 'videoinput' && device.label.toLowerCase().includes('back'));
+
+    if (!backCamera) {
+     setBackCameraExists(false)
+     console.log("no back camera")
+      return;
+    }}
+    cameraChecker()
+  }, []);
+
+
 
   const startRecording = async () => {
     try {
-
   setRecordedChunks([])
 // Capture video from the screen without audio
-  const videoStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+      // Specify the desired camera using the facingMode constraint
+    const constraints = {
+      video: {
+        facingMode: facingMode // 'user' for front camera, 'environment' for back camera
+      },
+      audio: true
+    };
+  const mergedStream = await navigator.mediaDevices.getUserMedia( constraints);
 
-      videoRef.current.srcObject = videoStream;
+      videoRef.current.srcObject = mergedStream;
   // Capture audio from the surrounding environment
-  const audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-  // Combine video and audio streams
-  const mergedStream = new MediaStream([
-    ...videoStream.getVideoTracks(),
-    ...audioStream.getAudioTracks()
-  ]);
-  const recorder = new MediaRecorder(mergedStream, { mimeType: 'video/webm' });
+
+      if (MediaRecorder.isTypeSupported('video/webm; codecs=vp9')) {
+    var options = {mimeType: 'video/webm; codecs=vp9'};
+    } else  if (MediaRecorder.isTypeSupported('video/webm')) {
+      var options = {mimeType: 'video/webm'};
+    } else if (MediaRecorder.isTypeSupported('video/mp4')) {
+    var options = {mimeType: 'video/mp4', videoBitsPerSecond : 100000};
+    } else {
+      alert("no suitable mimetype found for this device")
+    console.error("no suitable mimetype found for this device");
+}
+
+      const recorder = new MediaRecorder(mergedStream, options);
       recorder.ondataavailable = handleDataAvailable;
       recorder.start();
       setMediaRecorder(recorder);
@@ -42,6 +75,7 @@ const stopRecording = () => {
     }
     return null; // Set mediaRecorder to null
   });
+  alert("recording stopped")
   console.log("Recording stopped");
 };
 
@@ -68,7 +102,13 @@ function handleEventNameChange(e){
     }
     try{
       setUploading(true)
-    const response=await props.handleFileUpload(recordedChunks[0],"",eventName)
+      var response
+      if (isIOS){
+     response=await props.handleFileUpload(recordedChunks[0],eventName)
+      }else{
+     response=await props.handleFileUpload(recordedChunks[0],"",eventName)
+      }
+
     console.log(response.data.data)
     alert("sucessfully uploaded")
     } catch (error) {
@@ -80,16 +120,20 @@ function handleEventNameChange(e){
     }
 
   }
+ const swapCamera = () => {
+    setFacingMode(prev => (prev === 'user' ? 'environment' : 'user'));
+  };
 return(
     <>
     <div
 style={{display:'flex',backgroundColor:"#282828",flexDirection:'column',alignItems:
 'center' } } >
-    <video style={{height:'80vh',marginBottom:'1rem'}} ref={videoRef} playsinline autoPlay />
+    <video muted={true} style={{height:'80vh',marginBottom:'1rem'}} ref={videoRef} playsInline autoPlay />
     <div>
-      <Button style={{marginRight:'1rem'}} onClick={startRecording}>Start
-Recording</Button>
-      <Button onClick={stopRecording}>Stop Recording</Button>
+      <Button style={{marginRight:'1rem'}} onClick={startRecording}>StartRecording</Button>
+      <Button style={{marginRight:'1rem'}} onClick={stopRecording}>Stop Recording</Button>
+      {mediaRecorder && backCameraExists && <Button onClick={swapCamera}>Swap Camera</Button>}
+
       <br />
       <br />
       <FormControl placeholder="EventName" name="eventName" type="text" onChange={(e)=>handleEventNameChange(e)} value={eventName} />
